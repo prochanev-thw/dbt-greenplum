01484b5aede166e9b221a5609fa9e36586ee69df
Evgeniy Prochan <72522187+prochanev-thw@users.noreply.github.com>
2025-09-25 16:24:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ setup(
     },
     install_requires=[
         "dbt-core~={}".format(dbt_core_version),
-        "dbt-postgres~={}".format(package_version),
+        "dbt-postgres==1.9.1",
         "{}~=2.9".format(DBT_PSYCOPG2_NAME),
     ],
     zip_safe=False,
