b45f931a97f295ecf57ba9c2e9b411d178a8e846
Evgeniy Prochan <72522187+prochanev-thw@users.noreply.github.com>
2025-09-25 15:36:25 +0300

    Update adapter for dbt-core 1.10

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ import os
 import re
 import sys
 
-if sys.version_info < (3, 7):
+if sys.version_info < (3, 9):
     print("Error: dbt does not support this version of Python.")
-    print("Please upgrade to Python 3.7 or higher.")
+    print("Please upgrade to Python 3.9 or higher.")
     sys.exit(1)
 
 
@@ -100,7 +100,7 @@ setup(
     install_requires=[
         "dbt-core~={}".format(dbt_core_version),
         "dbt-postgres~={}".format(package_version),
-        "{}~=2.8".format(DBT_PSYCOPG2_NAME),
+        "{}~=2.9".format(DBT_PSYCOPG2_NAME),
     ],
     zip_safe=False,
     classifiers=[
@@ -109,12 +109,11 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.9",
 )
 
